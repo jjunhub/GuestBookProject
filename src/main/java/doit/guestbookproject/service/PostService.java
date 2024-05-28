d9680880e957830805f15387f9a884cd1965e4f8
sangjun <sangjun1389@ajou.ac.kr>
2024-05-28 15:48:06 +0900

    fix: Implement transcational for jpa dirty check

--- src/main/java/doit/guestbookproject/service/PostService.java
+++ src/main/java/doit/guestbookproject/service/PostService.java
@@ -11,9 +11,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
 
     private final PostRepository postRepository;
